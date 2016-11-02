a5571604a2c8f6ddb0a1f93955eab341878119e2
Dan Gorman <danielhgorman@gmail.com>
2016-11-02 18:00:17 -0400

    merged conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
 
-  root to: "home#index"
-=======
   devise_scope :user do
     root to: "home#index"
   end
->>>>>>> 79ba6f9003a19e89328bb8925ab5d19f73a2e37c
 end
