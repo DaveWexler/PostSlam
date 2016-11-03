47ddfae3bbe4802b933e250d48667ecfaf0d3d50
DaveWexler <dwexler@gmail.com>
2016-11-03 16:24:48 -0400

    add fb and twitter checkboxes to post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,18 +9,33 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    @post.twitter = true
-    @post.facebook = true
+    if post_params[:facebook] == "1"
+      send_to_facebook
+    end
+    if post_params[:twitter] == "1"
+      send_to_twitter
+    end
     @post.save
     flash[:notice] = "Post successfully created"
     redirect_to root_path
   end
 
-
   private
 
+  def send_to_facebook
+    Adapter::Xapix::Facebook.new(post_params)
+    # if successful:
+    @post.facebook = true
+  end
+
+  def send_to_twitter
+    Adapter::Xapix::Twitter.new(post_params)
+    # if successful
+    @post.twitter = true
+  end
+
   def post_params
-    params.require("post").permit(:title, :body)
+    params.require("post").permit(:title, :body, :facebook, :twitter)
   end
 
 end
