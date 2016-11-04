b57b2005c03a7824dcfcabaf9fe7034aaf04fe76
DaveWexler <dwexler@gmail.com>
2016-11-04 17:34:20 -0400

    twitter box hide and unhide works

--- app/assets/javascripts/twitter_counter.js
+++ app/assets/javascripts/twitter_counter.js
@@ -1,16 +1,17 @@
-// $(document).ready(function(){
-  // var chars_remaining = 140
-  // $('#char').text("" + chars_remaining)
-
 function update(text){
-  // $('#text-body').keyup(function(e){
-    var length = text.value.length
-    chars_remaining = 140 - length
-    $('#char').text("" + chars_remaining)
-    if (chars_remaining < 0) {
-
-    }
-
-  // })
+  var length = text.value.length
+  chars_remaining = 140 - length
+  $('#char').text("" + chars_remaining)
+  if (chars_remaining < 0) {
+    disableTwitter()
+  } else {
+    enableTwitter()
+  }
 }
-// })
+
+function disableTwitter () {
+  $('#twitter-box').attr('checked', false).css("visibility", "hidden")
+}
+function enableTwitter () {
+  $('#twitter-box').attr('checked', true).css("visibility", "visible")
+}
